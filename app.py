eb072734702ceb0121881def111cc1cf91289d3e
Ahmet Yusuf Başaran <aybasaran10@gmail.com>
2026-07-19 14:24:35 +0300

    feat: ekle logo setini ve pencere ikonunu

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import logging
 import sys
 
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 
 from Screens.Main import Main
 from Screens.theme import THEME_QSS
+from Utils.paths import asset_path
 
 __version__ = "1.0"
 __authors__ = ["Ahmet Yusuf Başaran ", "Yusufcan Günay"]
@@ -17,6 +19,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     # Koyu tema: tum ekranlar tek global QSS'ten beslenir
     app.setStyleSheet(THEME_QSS)
+    app.setWindowIcon(QIcon(asset_path("logo/icon-256.png")))
     # Ekran Cozunurluk degerlerini almak!
     geometry = app.primaryScreen().geometry()
     main_window = Main(geometry.width(), geometry.height())
